ccc281d1e9f89bb2a6c49ed8bec4383f3cc4e84d
RishabJha134 <rishabjha134@gmail.com>
2024-05-29 16:16:17 +0530

    complete project

--- content.js
+++ content.js
@@ -2,15 +2,15 @@
 const newProfilePicUrl =
   "https://s.ndtvimg.com/images/entities/300/virat-kohli-967.png";
 
-// Function to replace profile pictures
+
 function replaceProfilePictures() {
-  // Select all profile pictures in the feed
+ 
   const profilePics = document.querySelectorAll(
-    "img.feed-shared-actor__avatar-image, img.artdeco-entity-image--circle-2, img.entity-result__image--profile, img.pv-top-card-profile-picture__image, img., img.update-components-actor__avatar"
+    "img.feed-shared-actor__avatar-image, img.artdeco-entity-image--circle-2, img.entity-result__image--profile, img.pv-top-card-profile-picture__image, img.ivm-view-attr__img--centered, img.update-components-actor__avatar"
   );
 
   profilePics.forEach((pic) => {
-    // Change the src attribute to the new profile picture URL
+   
     console.log(pic.src);
     console.log(newProfilePicUrl);
     pic.src = newProfilePicUrl;
@@ -18,7 +18,7 @@ function replaceProfilePictures() {
   });
 }
 
-// Run the function to replace profile pictures
+
 replaceProfilePictures();
 
 // Set up a MutationObserver to detect changes in the feed
